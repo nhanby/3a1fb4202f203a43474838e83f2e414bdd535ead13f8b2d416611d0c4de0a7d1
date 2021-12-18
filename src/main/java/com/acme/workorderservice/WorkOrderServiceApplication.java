8f87fe3f6033b5b6cf9eac159d223219b4eb83ae
hanbyn <nigel.hanby@gmail.com>
2021-12-18 19:24:35 +0000

    initial project creation

---
+++ src/main/java/com/acme/workorderservice/WorkOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.acme.workorderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkOrderServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/acme/workorderservice/WorkOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.workorderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkOrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
